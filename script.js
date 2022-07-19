462fa73d7a03bd0143bffb03a95e5b0edd19f3d8
ReneP8 <rene-baving@web.de>
2022-07-19 12:35:34 +0200

    Fix: not saving new links

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ saveLink = function(link){
     var tmpLinks = [];
 
     chrome.storage.local.get(['lalLinks'], function(result) {
-        if (result && result.links) {
-            tmpLinks = result.links;
+        if (result && result.lalLinks) {
+            tmpLinks = result.lalLinks;
         }
         tmpLinks.push(link.linkUrl);
         chrome.storage.local.set({'lalLinks': tmpLinks}, function() {
             console.dir(tmpLinks);
         });   
       });
-
-       
 };
 
 chrome.contextMenus.removeAll(function() {
