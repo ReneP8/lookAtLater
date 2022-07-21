4e4cd21fd90878bc7a288ca2457a97fe72896b34
ReneP8 <rene-baving@web.de>
2022-07-21 11:45:02 +0200

    fixed not saving title into storage

--- public/script.js
+++ public/script.js
@@ -1,24 +1,25 @@
-saveLink = function (link) {
+saveLink = async function (link) {
     var tmpLinks = [];
 
     var title = "";
 
-    fetch(`https://title-for-url.herokuapp.com/api/title?url=${link.linkUrl}`)
+    await fetch(`https://title-for-url.herokuapp.com/api/title?url=${link.linkUrl}`)
         .then(response => response.json())
-        .then(data => title = data.title);
-
-    chrome.storage.local.get(['lalLinks'], function (result) {
-        if (result && result.lalLinks) {
-            tmpLinks = result.lalLinks;
-        }
-        tmpLinks.push({
-            title: title,
-            url: link.linkUrl
+        .then(data => {
+            title = data.title;
+            chrome.storage.local.get(['lalLinks'], function (result) {
+                if (result && result.lalLinks) {
+                    tmpLinks = result.lalLinks;
+                }
+                tmpLinks.push({
+                    title: title,
+                    url: link.linkUrl
+                });
+                chrome.storage.local.set({ 'lalLinks': tmpLinks }, function () {
+                    console.dir(tmpLinks);
+                });
+            });
         });
-        chrome.storage.local.set({ 'lalLinks': tmpLinks }, function () {
-            console.dir(tmpLinks);
-        });
-    });
 };
 
 chrome.contextMenus.removeAll(function () {
