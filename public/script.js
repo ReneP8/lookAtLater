f4ce4878f78d2dc50dbfbc568aae221b0a553df7
ReneP8 <Rene-Baving@web.de>
2022-09-22 15:02:32 +0200

    removed get title feature

--- public/script.js
+++ public/script.js
@@ -1,30 +1,32 @@
-saveLink = async function (link) {
+saveLink = async function(link) {
     var tmpLinks = [];
 
     var title = "New Link";
 
-    await fetch(`https://title-for-url.herokuapp.com/api/title?url=${link.linkUrl}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.title && data.title.length > 0) {
-                title = data.title;
-            }
-            chrome.storage.local.get(['lalLinks'], function (result) {
-                if (result && result.lalLinks) {
-                    tmpLinks = result.lalLinks;
-                }
-                tmpLinks.push({
-                    title: title,
-                    url: link.linkUrl
-                });
-                chrome.storage.local.set({ 'lalLinks': tmpLinks }, function () {
-                    console.dir(tmpLinks);
-                });
-            });
+    chrome.storage.local.get(['lalLinks'], function(result) {
+        if (result && result.lalLinks) {
+            tmpLinks = result.lalLinks;
+        }
+        tmpLinks.push({
+            title: title,
+            url: link.linkUrl
         });
+        chrome.storage.local.set({ 'lalLinks': tmpLinks }, function() {
+            console.dir(tmpLinks);
+        });
+    });
+
+    // await fetch(`https://title-for-url.herokuapp.com/api/title?url=${link.linkUrl}`)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         if (data.title && data.title.length > 0) {
+    //             title = data.title;
+    //         }
+
+    //     });
 };
 
-chrome.contextMenus.removeAll(function () {
+chrome.contextMenus.removeAll(function() {
     chrome.contextMenus.create({
         id: "1",
         title: "Look at Later!",
