698ae97273f5e12901a4b879c8302ff84a663557
ReneP8 <Rene-Baving@web.de>
2022-09-22 15:26:06 +0200

    added feature to save page url

--- public/script.js
+++ public/script.js
@@ -1,7 +1,14 @@
-saveLink = async function(link) {
+saveLink = async function(resource) {
     var tmpLinks = [];
-
     var title = "New Link";
+
+    var newUrl = "";
+
+    if (resource && resource.linkUrl) {
+        newUrl = resource.linkUrl;
+    } else if (resource && resource.pageUrl) {
+        newUrl = resource.pageUrl;
+    }
 
     chrome.storage.local.get(['lalLinks'], function(result) {
         if (result && result.lalLinks) {
@@ -9,26 +16,22 @@ saveLink = async function(link) {
         }
         tmpLinks.push({
             title: title,
-            url: link.linkUrl
+            url: newUrl
         });
         chrome.storage.local.set({ 'lalLinks': tmpLinks }, function() {
             console.dir(tmpLinks);
         });
     });
-
-    // await fetch(`https://title-for-url.herokuapp.com/api/title?url=${link.linkUrl}`)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         if (data.title && data.title.length > 0) {
-    //             title = data.title;
-    //         }
-
-    //     });
 };
 
 chrome.contextMenus.removeAll(function() {
     chrome.contextMenus.create({
         id: "1",
+        title: "Look at Later!",
+        contexts: ["page"],
+    });
+    chrome.contextMenus.create({
+        id: "2",
         title: "Look at Later!",
         contexts: ["link"],
     });
