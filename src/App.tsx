7d5b4f0b0996e4aecda2ac0db1de243e6c6b0f17
ReneP8 <Rene-Baving@web.de>
2022-09-22 14:40:27 +0200

    changed design for saved links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Container, Button, Col, Row, Stack } from 'react-bootstrap';
+import { useEffect, useState } from "react";
+import { Container, Button, Col, Row, Stack, Table } from "react-bootstrap";
 
 interface LalLink {
   title: string;
@@ -12,43 +12,59 @@ function App() {
 
   useEffect(() => {
     // Todo unlimited loop.
-    chrome.storage.local.get(['lalLinks'], function (result: any) {
+    chrome.storage.local.get(["lalLinks"], function (result: any) {
       let tmpLinks: LalLink[] = [];
-      tmpLinks = result.lalLinks ? result.lalLinks as LalLink[] : [];
+      tmpLinks = result.lalLinks ? (result.lalLinks as LalLink[]) : [];
       console.log(tmpLinks);
       setLinks(tmpLinks);
-    })
+    });
   }, [reload]);
 
   return (
     <Container className="m-3">
-      <h4>Meine Links</h4>
-      {links.length != 0 && links.map((link, index) => (
-        <Row className="my-1">
-          <Col><Row className="align-content-center"><p>{link.title}</p></Row></Col>
-          <Col></Col>
-          <Col>
-            <Row className="align-content-center">
-              <Stack direction="horizontal" gap={1}>
-                <Button size="sm" target="_blank" href={link.url}>Watch</Button>
-                <Button size="sm" onClick={async () => await deleteLink(index)}>Delete</Button>
-              </Stack>
-            </Row>
-          </Col>
-        </Row>
-      ))
-      }
+      <h4>My saved Links</h4>
+      {links.length !== 0 && (
+        <Table striped bordered size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>URL</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {links.map((link, index) => (
+              <tr>
+                <td>
+                  <p>{index + 1}</p>
+                </td>
+                <td>
+                  <p>{link.url}</p>
+                </td>
+                <td>
+                  <Button size="sm" target="_blank" href={link.url}>
+                    Watch
+                  </Button>{" "}
+                  <Button
+                    size="sm"
+                    onClick={async () => await deleteLink(index)}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
 
-      {links.length == 0 &&
-        <p>Keine Links verfügbar</p>
-      }
+      {links.length === 0 && <p>No links available</p>}
     </Container>
   );
 
   async function deleteLink(index: number) {
     links.splice(index, 1);
-    chrome.storage.local.set({ 'lalLinks': links }, () => {
-    });
+    chrome.storage.local.set({ lalLinks: links }, () => {});
     setLinks(links);
     setReload(!reload);
   }
